b4c1882a4fa7fa1277d8204ef764c99c4ff431d2
BestLine <Falllenman@hotmail.com>
2017-08-28 02:44:29 +0300

    что то там за коммит

---
+++ RadSim/Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Transform transform;
+    public Text text;
+    int score;
+
+    void FixedUpdate()
+    {
+        score = (int)transform.position.x;
+        text.text = "Score: " + score;
+    }
+}
