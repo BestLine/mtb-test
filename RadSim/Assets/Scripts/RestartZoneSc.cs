d2676ba6c4a17a7aa60b0964a0aad25d08d786fe
NewAge747 <31107194+NewAge747@users.noreply.github.com>
2017-08-28 06:47:51 +0300

    ГУСИНАЯСИЛА
    При падении лвл перезапускается

---
+++ RadSim/Assets/Scripts/RestartZoneSc.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class RestartZoneSc : MonoBehaviour {
+
+   
+
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if (coll.gameObject.tag == "ResZone")
+        
+            SceneManager.LoadScene("mainLevel");
+
+
+
+    }
+        }
