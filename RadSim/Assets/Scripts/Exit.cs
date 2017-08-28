3ff5db1bb468d885b1a4c332bb982aa37d533c7f
BestLine <Falllenman@hotmail.com>
2017-08-29 00:29:31 +0300

    добавил кнопку выхода

---
+++ RadSim/Assets/Scripts/Exit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour {
+	void goExit (bool go) {
+        if (go)
+            Application.Quit();
+    }
+}
